4f8a02fd80968903a165ec82a3fb8cbd3149dc90
Pramod <svpramodkumar1@gmail.com>
2025-03-10 13:35:55 +0530

    Initial commit

--- azure/main.tf
+++ azure/main.tf
@@ -73,4 +73,10 @@ resource "azurerm_network_interface" "nic" {
   name                = "multi-cloud-nic"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "ipconfig1"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
 }
