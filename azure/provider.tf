c6d32ccb7855ac6eea50289a40cf09af739ae56b
Pramod <svpramodkumar1@gmail.com>
2025-03-10 13:44:12 +0530

    Initial commit

--- azure/provider.tf
+++ azure/provider.tf
@@ -1,4 +1,8 @@
 provider "azurerm" {
   features {}
-}
 
+  subscription_id = "7165c109-ded4-40cb-9a42-5ee07fb16180"
+  client_id       = "11111111-1111-1111-1111-111111111111"
+  client_secret   = "22222222-2222-2222-2222-222222222222"
+  tenant_id       = "347d1228-8668-4cf4-bf10-bb7b3abf9ccc"
+}
