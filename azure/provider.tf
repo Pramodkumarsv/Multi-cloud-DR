069cb0cea647f0de7d28e01c283269cf1948937d
Pramod <svpramodkumar1@gmail.com>
2025-03-10 22:12:39 +0530

    updated code and added commands

--- azure/provider.tf
+++ azure/provider.tf
@@ -3,5 +3,5 @@ provider "azurerm" {
 
   use_cli = true
   tenant_id       = "347d1228-8668-4cf4-bf10-bb7b3abf9"
-  subscription_id = "347d1228-8668-4cf4-bf10-bb7b3abf9ccc"
+  subscription_id = "7165c109-ded4-40cb-9a42-5ee07fb16180"
 }
