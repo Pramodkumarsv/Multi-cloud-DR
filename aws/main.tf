8c6a60ac04baf009394e6811e1d08d21f47c83af
Pramod <svpramodkumar1@gmail.com>
2025-03-10 12:17:41 +0530

    Updated folder with latest changes

--- aws/main.tf
+++ aws/main.tf
@@ -59,7 +59,7 @@ resource "aws_security_group" "web_sg" {
 }
 
 resource "aws_key_pair" "keypair" {
-  key_name   = "my-key1"
+  key_name   = "my-key2"
   public_key = file("~/.ssh/id_rsa.pub")
 }
 
